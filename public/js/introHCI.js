bdbe48ac3132ad0881a87e5572dae2fe2b842efc
briannayamanaka <briannayamanaka@gmail.com>
2016-02-12 02:21:42 +0000

    first commit

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -2,31 +2,45 @@
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-	initializePage();
+  initializePage();
 })
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$('.project a').click(addProjectDetails);
+  $('.project a').click(addProjectDetails);
 
-	$('#colorBtn').click(randomizeColors);
+  $('#colorBtn').click(randomizeColors);
 }
 
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
 function addProjectDetails(e) {
-	// Prevent following the link
-	e.preventDefault();
+  // Prevent following the link
+  e.preventDefault();
 
-	// Get the div ID, e.g., "project3"
-	var projectID = $(this).closest('.project').attr('id');
-	// get rid of 'project' from the front of the id 'project3'
-	var idNumber = projectID.substr('project'.length);
+  // Get the div ID, e.g., "project3"
+  var projectID = $(this).closest('.project').attr('id');
+  // get rid of 'project' from the front of the id 'project3'
+  var idNumber = projectID.substr('project'.length);
 
-	console.log("User clicked on project " + idNumber);
+  console.log('/project/' + idNumber);
+  $.get('/project/' + idNumber, projectAjax);
+
+  console.log("User clicked on project " + idNumber);
+}
+
+function projectAjax(result) {
+  console.log(result);
+
+  $('#project'+result.id).find('.details').html(
+    '<h3>'+result.title+'</h3>' +
+    '<h4>'+result.date+'</h4>'+
+    '<img src="'+result.image+'" class="detailsImage">' +
+    result.summary
+  );
 }
 
 /*
@@ -34,5 +48,17 @@ function addProjectDetails(e) {
  * and apply it
  */
 function randomizeColors(e) {
-	console.log("User clicked on color button");
+  console.log("User clicked on color button");
+
+  $.get('palette', function(result) {
+    console.log(result);
+
+    var colors = result.colors.hex;
+
+    $('body').css('background-color', colors[0]);
+    $('.thumbnail').css('background-color', colors[1]);
+    $('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+    $('p').css('color', colors[3]);
+    $('.project img').css('opacity', 0.75);
+  });
 }
